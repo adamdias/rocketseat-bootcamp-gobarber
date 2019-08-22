d82cc2be46c6de763c1e34f6f3d0e98a80504409
Adam Dias <adam.dias@outlook.com>
2019-08-22 08:23:03 -0300

    Update .env

--- src/app/controllers/ProviderController.js
+++ src/app/controllers/ProviderController.js
@@ -3,7 +3,7 @@ import File from '../models/File';
 
 class ProviderController {
   async index(req, res) {
-    const providers = await User.finddAll({
+    const providers = await User.findAll({
       where: { provider: true },
       attributes: ['id', 'name', 'email', 'avatar_id'],
       include: [
